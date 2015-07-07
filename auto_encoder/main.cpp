4be361ec9b88fdfb98cfb43052d63f5bcdd07922
Ryo Miyajima <r.miyajima@aktsk.jp>
2015-07-07 13:31:01 +0900

    WORKED WITH ONE IMAGE

--- auto_encoder/main.cpp
+++ auto_encoder/main.cpp
@@ -15,11 +15,13 @@ int main(void) {
     srand(0);
     ActivationFunction rectifier(
         MatrixFunction([](double input) {
-            return 1.0 / (1.0 + exp(input));
+            return std::max<double>(input, 0);
         }),
         MatrixFunction([](double input) {
-            double exp_ = exp(input);
-            return exp_ / (1.0 + exp_) / (1.0 + exp_);
+            if (input < 0)
+                return 0;
+            else
+                return 1;
         }));
     ActivationFunction identity(
         MatrixFunction([](double input) {
@@ -33,12 +35,13 @@ int main(void) {
     const int numSamples = 100;
     const int firstLayerNodeNum = MnistIO::numRows * MnistIO::numCols;
     const int midLayerNodeNum = 100;
-    FirstLayer firstLayer(numBatches, firstLayerNodeNum, rectifier);
-    Layer midLayer(numBatches, midLayerNodeNum, rectifier, &firstLayer);
-    LastLayer lastLayer(numBatches, firstLayerNodeNum, identity, &midLayer);
+    FirstLayer firstLayer(1, firstLayerNodeNum, rectifier);
+    Layer midLayer(1, midLayerNodeNum, rectifier, &firstLayer);
+    LastLayer lastLayer(1, firstLayerNodeNum, identity, &midLayer);
 
     std::vector<Matrix> inputs = MnistIO::MnistToMatrixInBatches("../mnist_loader/t10k-images-idx3-ubyte", numBatches);
 
+    /*
     Matrix average(firstLayerNodeNum, 1);
     for (int batchIndex = 0; batchIndex < numBatches; batchIndex++) {
     for (int sampleIndex = 0; sampleIndex < numSamples; sampleIndex++) {
@@ -51,9 +54,15 @@ int main(void) {
     for (int pixelIndex = 0; pixelIndex < firstLayerNodeNum; pixelIndex++) {
         inputs.at(batchIndex)(pixelIndex, sampleIndex) -= average(pixelIndex);
     }}}
+    */
+
+    Matrix input(firstLayerNodeNum, 1);
+    for (int pixelIndex = 0; pixelIndex < firstLayerNodeNum; pixelIndex++) {
+        input(pixelIndex, 0) = inputs.front()(pixelIndex, 0);
+    }
 
     for (int batchIndex = 0; batchIndex < numBatches; batchIndex++) {
-        const Matrix& input = inputs.at(batchIndex);
+        // const Matrix& input = inputs.at(batchIndex);
         std::cout << "batch index" << batchIndex << std::endl;
         // forward propagation
         Matrix Y = lastLayer.forwardPropagation(
