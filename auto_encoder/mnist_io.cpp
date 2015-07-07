0d9d82d9ecd902c2a3349ca6693dc3bcc5380125
Ryo Miyajima <r.miyajima@aktsk.jp>
2015-07-07 09:44:49 +0900

    implement mnist_io for batch

--- auto_encoder/mnist_io.cpp
+++ auto_encoder/mnist_io.cpp
@@ -2,6 +2,7 @@
 #include "png_generator.h"
 #include "mnist_loader.h"
 #include <math.h>
+#include <assert.h>
 
 namespace MnistIO {
 void MatrixToPNG(const Matrix& matrix) {
@@ -9,6 +10,15 @@ void MatrixToPNG(const Matrix& matrix) {
     PngWriter writer(numRows, numCols);
     for (int row = 0; row < matrix.getRow(); row++) {
         uint8_t byte = static_cast<uint8_t>(round(matrix(row) * max));
+        writer.addPixel(byte);
+    }
+    writer.write("out.png");
+}
+void MatrixToPNG(const Matrix& matrix, int sampleIndex) {
+    static const double max = static_cast<double>(std::numeric_limits<uint8_t>::max());
+    PngWriter writer(numRows, numCols);
+    for (int row = 0; row < matrix.getRow(); row++) {
+        uint8_t byte = static_cast<uint8_t>(round(matrix(row, sampleIndex) * max));
         writer.addPixel(byte);
     }
     writer.write("out.png");
@@ -23,11 +33,27 @@ std::vector<Matrix> MnistToMatrix(const std::string& filename) {
     }}
     return ret;
 }
+std::vector<Matrix> MnistToMatrixInBatches(const std::string& filename, int numSamples) {
+    static const double max = static_cast<double>(std::numeric_limits<uint8_t>::max());
+    MnistLoader mnistLoader(filename);
+    assert(numImages % numSamples == 0);
+    const int numBatches = numImages / numSamples;
+    std::vector<Matrix> ret(numBatches, Matrix(numRows * numCols, numSamples));
+    int pixelIndexAccumulator = 0;
+    for (int batchIndex = 0; batchIndex < numBatches; batchIndex++) {
+    for (int sampleIndex = 0; sampleIndex < numSamples; sampleIndex++) {
+    for (int pixelIndex = 0; pixelIndex < numRows * numCols; pixelIndex++) {
+        ret.at(batchIndex)(pixelIndex, sampleIndex) = static_cast<double>(mnistLoader.getPixel(pixelIndexAccumulator++)) / max;
+    }}}
+    return ret;
+}
 }
 
 // for testing
+/*
 int main(void) {
-    std::vector<Matrix> mat = MnistIO::MnistToMatrix("../mnist_loader/t10k-images-idx3-ubyte");
-    MnistIO::MatrixToPNG(mat.at(3));
+    std::vector<Matrix> mat = MnistIO::MnistToMatrixInBatches("../mnist_loader/t10k-images-idx3-ubyte", 100);
+    MnistIO::MatrixToPNG(mat.at(99), 99);
     return 0;
 }
+*/
--- auto_encoder/mnist_io.h
+++ auto_encoder/mnist_io.h
@@ -6,7 +6,9 @@
 
 namespace MnistIO {
     void MatrixToPNG(const Matrix& matrix);
+    void MatrixToPNG(const Matrix& matrix, int sampleIndex);
     std::vector<Matrix> MnistToMatrix(const std::string& filename);
+    std::vector<Matrix> MnistToMatrixInBatches(const std::string& filename, int numSamples);
     static const int numRows = 28;
     static const int numCols = 28;
     static const int numImages = 10000;
